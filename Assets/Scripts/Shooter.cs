16e9ab01dec66494fb49e79e1ceb727268453022
mjpurin <mjpurin@gmail.com>
2023-02-07 14:18:29 +0900

    p194まで作成

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    public GameObject[] candyPrefabs;
+    public Transform candyParentTransform;
     public GameObject candyPrefab;
     public float shotForce;
     public float shotToruque;
+    public float baseWidth;
 
     void Update()
     {
         if(Input.GetButtonDown("Fire1")) Shot();
         
     }
+
+    GameObject SampleCandy(){
+        int index=Random.Range(0,candyPrefabs.Length);
+        return candyPrefabs[index];
+    }
+    Vector3 GetInstantiatePosition(){
+        float x = baseWidth *
+        (Input.mousePosition.x/Screen.width) -(baseWidth/2);
+        return transform.position+new Vector3(x,0,0);
+    }
     public void Shot(){
         GameObject candy=Instantiate(
-            candyPrefab,
-            transform.position,
+            SampleCandy(),
+            GetInstantiatePosition(),
             Quaternion.identity
         );
+
+        candy.transform.parent=candyParentTransform;
 
         Rigidbody candyRigidbody=candy.GetComponent<Rigidbody>();
         candyRigidbody.AddForce(transform.forward * shotForce);
